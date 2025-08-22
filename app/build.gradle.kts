8b799513954b7227b9be976a1cc1e050272887ca
Jakub Piasecki <jakub.piasecki@swmansion.com>
2025-08-22 09:45:24 +0200

    Init app

---
+++ app/build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "io.github.jpiasecki.truncationtest"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "io.github.jpiasecki.truncationtest"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        ndk {
+            abiFilters += "x86"
+        }
+
+        externalNativeBuild {
+            cmake {
+                cFlags += "-O3 -m32"
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
+        }
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ app/src/main/cpp/native-lib.c
@@ -0,0 +1,24 @@
+#include <jni.h>
+#include <stdio.h>
+#include <android/log.h>
+
+volatile double D = 0xFFFFFFFF;
+
+char* test() {
+  char *buffer = NULL;
+  volatile int x = (int)D;
+  asprintf(&buffer, "%f %x\n", D, x);
+  return buffer;
+}
+
+JNIEXPORT jstring JNICALL
+Java_io_github_jpiasecki_truncationtest_MainActivity_stringFromJNI(
+    JNIEnv* env,
+    jobject thiz) {
+  char* result = test();
+
+  __android_log_print(ANDROID_LOG_ERROR, "TRUNCATION", "%s", result);
+
+  jstring jresult = (*env)->NewStringUTF(env, result);
+  return jresult;
+}
